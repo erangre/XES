429a847c0e668252a485618d97d344b9753ca2a6
Eran Greenberg <erangre@gmail.com>
2018-03-06 18:36:47 -0600

    added function to calculate d_hkl

--- xes/model/XESModel.py
+++ xes/model/XESModel.py
@@ -3,12 +3,13 @@ import math
 import numpy as np
 from qtpy import QtCore
 
-SI_a = 5.431E-10
+Si_a = 5.431E-10
 Si_h = 4
 Si_k = 4
 Si_l = 0
 h = 4.135667516E-15
 c = 299792458
+
 
 class XESModel(QtCore.QObject):
     # my_signal = QtCore.Signal()
@@ -16,14 +17,19 @@ class XESModel(QtCore.QObject):
     def __init__(self):
         super(XESModel, self).__init__()
 
-    @staticmethod
-    def theta_to_ev(theta):
-        lam = 2.0 * SI_a / np.sqrt(Si_h**2 + Si_k**2 + Si_l**2) * np.sin(np.pi*theta/180.0)
+    def theta_to_ev(self, theta):
+        d_hkl = self.d_hkl(Si_a, Si_h, Si_k, Si_l)
+        lam = 2.0 * d_hkl * np.sin(np.pi*theta/180.0)
         ev = h*c/lam
         return ev
 
-    @staticmethod
-    def ev_to_theta(ev):
-        sin_theta = h*c/ev/2.0/(SI_a/np.sqrt(Si_h**2 + Si_k**2 + Si_l**2))
+    def ev_to_theta(self, ev):
+        d_hkl = self.d_hkl(Si_a, Si_h, Si_k, Si_l)
+        sin_theta = h*c/ev/2.0/d_hkl
         theta = 180.0 * np.arcsin(sin_theta)/np.pi
         return theta
+
+    @staticmethod
+    def d_hkl(a, hh, kk, ll):
+        d = a / np.sqrt(hh**2 + kk**2 + ll**2)
+        return d
